9b563341106897c47bf80cf5f8bd29ea368b5df9
L3ni0 <56505968+L3ni0@users.noreply.github.com>
2025-12-14 23:30:46 +0100

    added games to bronze layer

--- dags/purchased_dag.py
+++ dags/purchased_dag.py
@@ -24,6 +24,12 @@ def games_dag():
         packages="org.postgresql:postgresql:42.7.3",
     )
 
+    load_games_data = SparkSubmitOperator(
+        task_id="load_games_to_bronze",
+        application="scripts/spark/bronze/games.py",
+        conn_id="spark_conn",
+        packages="org.postgresql:postgresql:42.7.3",
+    )
     clear_purchases_data = SparkSubmitOperator(
         task_id="clear_purchased",
         application="scripts/spark/silver/purchased_games.py",
@@ -53,6 +59,7 @@ def games_dag():
 
     start >> load_purchases_data >> clear_purchases_data >> aggregate_purchases_data
     start >> load_players_data >> clear_players_data >> aggregate_players_data
+    start >> load_games_data
 
 
 games_dag()
