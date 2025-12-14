0efe79ef95604f642f24c26efaa52402a7da289e
L3ni0 <56505968+L3ni0@users.noreply.github.com>
2025-12-14 23:03:46 +0100

    fixed spark app name

--- scripts/spark/gold/players.py
+++ scripts/spark/gold/players.py
@@ -6,7 +6,7 @@ from pyspark.sql.dataframe import DataFrame
 
 def create_spark_session() -> SparkSession:
     spark = (
-        SparkSession.builder.appName("Purchases Gold")
+        SparkSession.builder.appName("Players Gold")
         .config("spark.jars.packages", "org.postgresql:postgresql:42.7.3")
         .getOrCreate()
     )
